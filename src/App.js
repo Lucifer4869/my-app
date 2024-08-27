be3cc542864586a9b809ced814fe7acafbc4a4d5
Lucifer4869 <band4869@gmail.com>
2024-08-27 11:20:07 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 function App() {
-  const [image, setimage] = useState('https://imgcp.aacdn.jp/img-a/600/auto/global-aaj-front/article/2017/10/59e042b5de07d_59e041ee14c8b_842368218.jpg');  // Initialize state with the word 'Hello'
+  const [image, setimage] = useState('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeUJpO2PMc1tcPT4c7ZDslMOo-zpXV29QkNQ&s'); 
+  const [word,setword] = useState("cat");
 
-  const toggleWord = () => {
-    // Use an if statement to toggle between the words 'Hello' and 'Goodbye'
-    if (image === 'https://imgcp.aacdn.jp/img-a/600/auto/global-aaj-front/article/2017/10/59e042b5de07d_59e041ee14c8b_842368218.jpg') {
-      setWord('https://imgcp.aacdn.jp/img-a/600/auto/global-aaj-front/article/2017/10/59e042b5de07d_59e041ee14c8b_842368218.jpg');
+  const toggleImage = () => {
+    
+    if (image === 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeUJpO2PMc1tcPT4c7ZDslMOo-zpXV29QkNQ&s') {
+      setimage('https://imgcp.aacdn.jp/img-a/600/auto/global-aaj-front/article/2017/10/59e042b5de07d_59e041ee14c8b_842368218.jpg');
+      setword ("dog");
+      
     } else {
-      setWord('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeUJpO2PMc1tcPT4c7ZDslMOo-zpXV29QkNQ&s');
+      setimage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeUJpO2PMc1tcPT4c7ZDslMOo-zpXV29QkNQ&s');
+      setword ("cat");
     }
   };
 
@@ -15,6 +19,7 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       {/* Display the current image */}
       <img src={image} alt="Toggle" style={{ width: '50%', maxWidth: '500px' }} />
+       <p>{word}</p>
       {/* Button to toggle the image */}
       <div>
         <button onClick={toggleImage} style={{ marginTop: '20px' }}>Toggle Image</button>
